6ed9792be2e770c9a07c2c6423f2877230724ca7
Nolacio-adm <carlos.nolacio@hotmail.com>
2026-04-30 13:11:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 const CACHE_NAME = 'scanner-v1';
 const ASSETS = [
-  'index.html',
-  'manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', e => {
@@ -9,5 +10,9 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('fetch', e => {
+  // CORREÇÃO: Ignorar requisições para o domínio do Google Script
+  if (e.request.url.includes('script.google.com')) {
+    return; // Deixa o navegador lidar diretamente, sem passar pelo cache
+  }
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });

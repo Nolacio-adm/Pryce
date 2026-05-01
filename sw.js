31535c94518d801abfdc21828d5018abae488fc9
Nolacio-adm <carlos.nolacio@hotmail.com>
2026-05-01 14:15:53 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,17 +2,19 @@ const CACHE_NAME = 'scanner-v1';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  'https://unpkg.com/html5-qrcode',
+  'https://unpkg.com/vanilla-masker@1.2.0/build/vanilla-masker.min.js'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  // Ignora chamadas ao Google Script para não quebrar o envio
-  if (e.request.url.includes('script.google.com')) {
-    return;
-  }
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
